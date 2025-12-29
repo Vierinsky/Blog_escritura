424ee3a9056152be8097e1beaa8ac811c17473b7
Felipe <felipe.viera@mayor.cl>
2025-12-29 17:35:48 -0300

    Creating a golbal SessionProvider

--- blog_escritura/app/layout.tsx
+++ blog_escritura/app/layout.tsx
@@ -1,6 +1,22 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
+import { Children } from "react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
